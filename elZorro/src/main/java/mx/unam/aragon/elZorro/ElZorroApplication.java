4019446c499cf6e1e928fcc432e8e301a247bb7b
FMM28 <marquez.maya.francisco.28@gmail.com>
2025-05-06 21:27:03 -0600

    creacion del proyecto

---
+++ elZorro/src/main/java/mx/unam/aragon/elZorro/ElZorroApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.elZorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElZorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElZorroApplication.class, args);
+	}
+
+}
