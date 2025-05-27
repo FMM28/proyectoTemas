bc7b3a5953f7803dea1754500281680b27a7c9e3
FMM28 <marquez.maya.francisco.28@gmail.com>
2025-05-27 11:28:41 -0600

    limpieza

--- elZorro/src/main/java/mx/unam/aragon/elZorro/config/AppConfig.java
+++ elZorro/src/main/java/mx/unam/aragon/elZorro/config/AppConfig.java
@@ -2,14 +2,12 @@ package mx.unam.aragon.elZorro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
--- elZorro/src/main/java/mx/unam/aragon/elZorro/controller/flask/FlaskController.java
+++ elZorro/src/main/java/mx/unam/aragon/elZorro/controller/flask/FlaskController.java
@@ -1,13 +1,8 @@
 package mx.unam.aragon.elZorro.controller.flask;
 
-import mx.unam.aragon.elZorro.model.entity.CategoriaEntity;
-import mx.unam.aragon.elZorro.model.entity.ProductoEntity;
-import mx.unam.aragon.elZorro.model.entity.ProveedorEntity;
 import mx.unam.aragon.elZorro.model.json.Inventario;
 import mx.unam.aragon.elZorro.security.FlaskRequestSender;
-import mx.unam.aragon.elZorro.service.inventario.InventarioService;
 import mx.unam.aragon.elZorro.service.inventario.InventarioServiceImpl;
-import mx.unam.aragon.elZorro.service.producto.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -15,11 +10,9 @@ import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
@@ -38,14 +31,12 @@ public class FlaskController {
     public ResponseEntity<Resource> generaExcel() {
         List<Inventario> productos = inventarioService.getAll();
 
-        // Verificar que la lista no esté vacía
         if(productos == null || productos.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No hay productos en el inventario");
         }
 
         ResponseEntity<byte[]> response = requestSender.postToFlask("/api/generar_excel", productos);
 
-        // Extraer nombre de archivo o usar uno por defecto
         String fileName = Optional.ofNullable(response.getHeaders().getContentDisposition())
                 .map(ContentDisposition::getFilename)
                 .orElse("inventario_" + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE) + ".xlsx");
